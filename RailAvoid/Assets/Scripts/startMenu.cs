cbd00ad34e0b005ea86a4dec4d46851ce3f34f41
Keston Gregory <Keston@Kestons-MacBook-Pro.local>
2014-10-08 14:46:18 -0400

    fixed buttons on menu, start button goes to game
    when you click start it goes to the game, when you click quit it quits
    the game (only in a build) **we should rename the scenes though to
    something better than “testingplayermove” **

--- RailAvoid/Assets/Scripts/startMenu.cs
+++ RailAvoid/Assets/Scripts/startMenu.cs
@@ -22,18 +22,18 @@ public class startMenu : MonoBehaviour
 	{
 		GUI.skin = startSkin;
 	
-		if (GUI.Button(new Rect(Screen.width/2 - 250,Screen.height/2 - 150,500,200), ""))
+		if (GUI.Button(new Rect(Screen.width/2 - 150,Screen.height/2 - 150,300,150), ""))
 		{
 			Debug.Log("you clicked the start button");
-			//Application.LoadLevel(1);
+			Application.LoadLevel("TestingPlayerMove");
 		}
 
 		GUI.skin = quitSkin;
 
-		if (GUI.Button(new Rect(Screen.width/2 - 250,Screen.height/2 + 50,500,200), ""))
+		if (GUI.Button(new Rect(Screen.width/2 - 150,Screen.height/2 + 50,300,150), ""))
 		{
 			Debug.Log("you clicked the quit button");
-			//Application.LoadLevel(1);
+			Application.Quit();
 		}
 	}
 }
