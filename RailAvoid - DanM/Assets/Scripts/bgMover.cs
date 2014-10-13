88c58d37ff2ddf56de015b5eccb07a282fc89c07
DanielMorelli <leo-iv@hotmail.com>
2014-10-13 04:11:57 -0400

    Crisis Averted, More Updates
    I synced up my folder with the latest updates, so the main menu and
    enemy ships are in and implemented. However the UI for the main menu
    doesn't seem to be working (I did set up a build. I even tested it on
    the other version and no buttons came up) so we might need to go over
    this live on Wednesday.
    
    Other than that, the issues I had with the earlier sync up seems to be
    resolved and nothing seems to be missing. I'm sorry for the scare.

---
+++ RailAvoid - DanM/Assets/Scripts/bgMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgMover : MonoBehaviour 
+{
+	GameObject bgPlanes;
+	float bgX;
+	float bgY;
+	bool checker = false;
+	public float bgScrollRate = 8;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bgPlanes = GameObject.Find("bgPlane");
+		bgX = bgPlanes.transform.position.x;
+		bgY = bgPlanes.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log(bgY);
+		transform.Translate(Vector3.up * Time.deltaTime / bgScrollRate, Space.World);
+	}
+}
---
+++ RailAvoid - DanM/Assets/Scripts/startMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startMenu : MonoBehaviour 
+{
+	public GUISkin startSkin;
+	public GUISkin quitSkin;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI () 
+	{
+		GUI.skin = startSkin;
+	
+		if (GUI.Button(new Rect(Screen.width/2 - 150,Screen.height/2 - 100,300,150), ""))
+		{
+			Debug.Log("you clicked the start button");
+			Application.LoadLevel("TestingPlayerMove");
+		}
+
+		GUI.skin = quitSkin;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 150,Screen.height/2 + 50,300,150), ""))
+		{
+			Debug.Log("you clicked the quit button");
+			Application.Quit();
+		}
+	}
+}
